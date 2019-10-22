1073289708aeb50d6bd0e8983dae5a4438673249
Gonzalo Campos <erick.campos@wizeline.com>
2019-10-21 22:22:54 -0500

    Initial commit.

---
+++ app/src/main/java/com/gonz/mx/repo/pattern/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gonz.mx.repo.pattern
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
